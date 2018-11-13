fe39277e2ec7f005445218e6105dd9ad71e6bb56
Mikel-Crotch <45015928+Mikel-Crotch@users.noreply.github.com>
2018-11-13 15:35:17 -0500

    Update U.S.B (1).user.js

--- U.S.B (1).user.js
+++ U.S.B (1).user.js
@@ -14,13 +14,13 @@
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$credit'){
-        sendMsg('My daddy Mikel and Angelos_Service had a baby and that baby was me (thank you Angelo for the code! :D)')
+        sendMsg('~My daddy Mikel and Angelos_Service had a baby and that baby was me (thank you Angelo for the code! :D)')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'r u ok?'){
-        sendMsg('I think?')
+        sendMsg('~I think?')
     }
 })
     socket.on('message',function(data){
@@ -32,152 +32,153 @@
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$help'){
-        sendMsg('"$credit"-credits, "$v"-U.S.B version, "r u ok?"-makes the bot say I think?')
+        sendMsg('~"$credit"-credits, "$v"-U.S.B version, "r u ok?"-makes the bot say I think?')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo pixel'){
-        sendMsg('ah yes pixel... idk anything about him')
+        sendMsg('~ah yes pixel... idk anything about him')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo frank'){
-        sendMsg('Oh yeah frank is my best friend, idk about my dad but frank is chill. Her cat has a tendancy to steal peperoni.')
+        sendMsg('~Oh yeah frank is my best friend, idk about my dad but frank is chill. Her cat has a tendancy to steal peperoni.')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo will'){
-        sendMsg('Idk if he is even here! he is oh so quiet!')
-        setTimeout(sendMsg('I can not tell if he is here or not') , 1010);
+          setTimeout(sendMsg('~I can not tell if he is here or not'), 1010);
+        sendMsg('~Idk if he is even here! he is oh so quiet!')
+      
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo wynn'){
-        sendMsg('"A Roleplayer" what ever that means... also fond of waluigi')
+        sendMsg('~"A Roleplayer" what ever that means... also fond of waluigi')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo lw82'){
-        sendMsg('He is cool but very quiet, some day i wanna grow big and strong like his bot!')
+        sendMsg('~He is cool but very quiet, some day i wanna grow big and strong like his bot!')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo Nutshinou'){
-        sendMsg('My nutterbutter. Really chill person look up their name in the video tab of google they have youtube!')
+        sendMsg('~My nutterbutter. Really chill person look up their name in the video tab of google they have youtube!')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo smith'){
-        sendMsg('A friend of my dad"s irl! He is a child so he needs his dad to "protect him". see frank for more details')
+        sendMsg('~A friend of my dad"s irl! He is a child so he needs his dad to "protect him". see frank for more details')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo 87'){
-        sendMsg('zaid, blue dot, 87. I do not know him well but he has dank discord profile pic')
+        sendMsg('~zaid, blue dot, 87. I do not know him well but he has dank discord profile pic')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo trees tree'){
-        sendMsg('I wanna be just like him!!!!! (a bot owned by lw82)')
+        sendMsg('~I wanna be just like him!!!!! (a bot owned by lw82)')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo sonic'){
-        sendMsg('The immposible name why would you even try to say that?')
+        sendMsg('~The immposible name why would you even try to say that?')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo mira'){
-        sendMsg('*kicks**kicks harder* /kao')
+        sendMsg('~*kicks**kicks harder* /kao')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo mikel crotch'){
-        sendMsg('The spammer, commedian, and ... i do not have anything else to say. he is my dad i guess?')
+        sendMsg('~The spammer, commedian, and ... i do not have anything else to say. he is my dad i guess?')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo hunter'){
-        sendMsg('angels bff and partners in crime')
+        sendMsg('~angels bff and partners in crime')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo angel'){
-        sendMsg('hunters bff and partners in crime')
+        sendMsg('~hunters bff and partners in crime')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo dragsun'){
-        sendMsg('...')
+        sendMsg('~idk about dragsun')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == '$sudo wenego'){
-        sendMsg('WARNING IP LOGGER DETECTED TAKE COVER TO NOT CLICK LINKS, NUTSHINOU WILL NEVER SEND LINKS!!!!')
+        sendMsg('~WARNING IP LOGGER DETECTED TAKE COVER TO NOT CLICK LINKS, NUTSHINOU WILL NEVER SEND LINKS!!!!')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'hi'){
-        sendMsg('Hello')
+        sendMsg('~Hello')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'howdy'){
-        sendMsg('Hello')
+        sendMsg('~Hello')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'hello'){
-        sendMsg('Hello')
+        sendMsg('~Hello')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'is this real?'){
-        sendMsg('Yes, this bot is programmed to say this chat isreal')
+        sendMsg('~Yes, this bot is programmed to say this chat isreal')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'is this real'){
-        sendMsg('Yes, this bot is programmed to say this chat isreal')
+        sendMsg('~Yes, this bot is programmed to say this chat isreal')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'is this chat real?'){
-        sendMsg('Yes, this bot is programmed to say this chat isreal')
+        sendMsg('~Yes, this bot is programmed to say this chat isreal')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'is this chat real'){
-        sendMsg('Yes, this bot is programmed to say this chat isreal')
+        sendMsg('~Yes, this bot is programmed to say this chat isreal')
     }
 })
     socket.on('message',function(data){
       data.msg = data.msg.trim()
     if(data.msg.toLowerCase() == 'spam'){
-        sendMsg('do /sin off')
+        sendMsg('~do /sin off')
     }
 })
  var i= ['+20']
