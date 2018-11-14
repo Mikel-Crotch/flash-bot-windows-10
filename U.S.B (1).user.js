168ae2b93a0de603716c3265a212e2cab43cdc5d
Mikel-Crotch <45015928+Mikel-Crotch@users.noreply.github.com>
2018-11-14 17:47:03 -0500

    Update U.S.B (1).user.js

--- U.S.B (1).user.js
+++ U.S.B (1).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         U.S.B
 // @namespace    http://tampermonkey.net/
-// @version      4.8
+// @version      5.0
 // @description  Hi i am Mikels son, as you imagine i am the spawn of satan!
 // @author       Mikel Crotch
 // @match        http://www.windows93.net/trollbox/
@@ -12,7 +12,7 @@
     'use strict';
 
     //initialization
-    var z
+    var z = 1;
     setTimeout(sendMsg('~Starting'), 1010)
     setTimeout(sendMsg('/sin off'), 1010)
     setTimeout(sendMsg('/say off'), 1010)
@@ -22,147 +22,148 @@
     socket.on('message',function(data){
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$credit'){
-            z + 1
+           (z ++);
             sendMsg('~My daddy Mikel and lw82 spent a long time together one night... and i was the made! (thank you Angelo for the code! :D, lw thanks for your help) msg='+z+'')}
         //r u ok?
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'r u ok?'){
-            z + 1
+            (z ++);
             sendMsg('~I think? msg='+z+'')}
         //$version
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$v'){
-            z + 1
+            (z ++);
             sendMsg('4.5 msg='+z+'')}
         //$help
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$help'){
-            z + 1
+            (z ++);
             sendMsg('~$credit-credits, $v-Bot version, r u ok?-triggers test response,$sudo [username]-to get USB’s outlook, $time-well to see time? msg='+z+'')}
         //pixel
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo pixel'){
-            z + 1
+            (z ++);
             sendMsg('~ah yes pixel... idk anything about him. msg='+z+'')}
         //frank
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo frank'){
-            z + 1
+            (z ++);
             sendMsg('~Oh yeah frank is my best friend, but prob not friends with my dad.frank is chill. Her cat also has a tendancy to steal peperoni. msg='+z+'')}
         //will
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo will'){
-            z + 1
+            (z ++);
             setTimeout(sendMsg('~I can not tell if he is here or not'), 1010);
             sendMsg('~Idk if he is even here! he is oh so quiet! msg='+z+'')}
         //wynn
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo wynn'){
-            z + 1
+            (z ++);
             sendMsg('~"A Roleplayer" what ever that means... also fond of waluigi. msg='+z+'')}
         //lw82
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo lw82'){
-            z + 1
+            (z ++);
             sendMsg('~He is cool but very quiet, some day i wanna grow big and strong like his bot! msg='+z+'')}
         //Nutshinou
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo nutshinou'){
-            z + 1
+            (z ++);
             sendMsg('~My nutterbutter. Really chill person look up their name in the video tab of google they have youtube! msg='+z+'')}
         //smith
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo smith'){
-            z + 1
+            (z ++);
             sendMsg('~A friend of my dad’s irl! He is a child so he needs his dad to "protect him". see frank for more details. msg='+z+'')}
         //87
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo 87'){
-            z + 1
+            (z ++);
             sendMsg('~zaid, blue dot, 87. I do not know him well but he has dank discord profile pic. msg='+z+'')}
         //trees tree
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo trees tree'){
-            z + 1
+            (z ++);
             sendMsg('~I wanna be just like him!!!!! (a bot owned by lw82) msg='+z+'')}
         //sonic
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo sonic'){
-            z + 1
+            (z ++);
             sendMsg('~The immposible name why would you even try to say that? msg='+z+'')}
         //Mira
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo mira'){
-            z + 1
+            (z ++);
             sendMsg('~*kicks**kicks harder* /kao. msg='+z+'')}
         //Teh trollbox mastter mikel crotch
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo mikel crotch'){
-            z + 1
+            (z ++);
             sendMsg('~The spammer, commedian, and ... i do not have anything else to say. he’s my dad i guess? msg='+z+'')}
         //hunter
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo hunter'){
-            z + 1
+            (z ++);
             sendMsg('~angel’s bff and partners in crime. msg='+z+'')}
         //angel
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo angel'){
-            z + 1
+            (z ++);
             sendMsg('~hunter’s bff and partners in crime. msg='+z+'')}
         //dragsun
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo dragsun'){
-            z + 1
+            (z ++);
             sendMsg('~idk about dragsun. msg='+z+'')}
         //wendego
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$sudo wenego'){
-            z + 1
+            (z ++);
             sendMsg('~WARNING IP LOGGER DETECTED TAKE COVER TO NOT CLICK LINKS, NUTSHINOU WILL NEVER SEND LINKS!!!! msg='+z+'')}
         //greating
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'hi'){
-            z + 1
+            (z ++);
             sendMsg('~Hello')}
         //greating
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'howdy'){
-            z + 1
+            (z ++);
             sendMsg('~Hello')}
         //greating
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'hello'){
-            z + 1
+            (z ++);
             sendMsg('~Hello')}
         //isreal
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'is this real?'){
-            z + 1
+            (z ++);
             sendMsg('~Yes, this bot is programmed to say this chat isreal')}
         //isreal
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'is this real'){
-            z + 1
+            (z ++);
             sendMsg('~Yes, this bot is programmed to say this chat isreal')}
         //isreal
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'is this chat real?'){
-            z + 1
+            (z ++);
             sendMsg('~Yes, this bot is programmed to say this chat isreal')}
         //isreal
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'is this chat real'){
-            z + 1
+            (z ++);
             sendMsg('~Yes, this bot is programmed to say this chat isreal')}
         //sin off advice
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == 'spam'){
-            z + 1
+            (z ++);
             sendMsg('~do /sin off my dude. msg='+z+'')}
         //time
         data.msg = data.msg.trim()
         if(data.msg.toLowerCase() == '$time'){
+            (z ++);
             var time = new Date()
             var h = time.getHours()
             var m = time.getMinutes()
